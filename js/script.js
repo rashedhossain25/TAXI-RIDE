74f389fc7d95089f7b0bf75481dee2c9070c2ad7
Rashed Hossain <145587722+rashedhossain25@users.noreply.github.com>
2024-03-04 18:33:56 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//js
+// aos 
+
+AOS.init({
+    
+    delay: 1,
+    duration: 500,
+    easing: 'linear',
+  
+  });
+//jq
+$(document).ready(function(){
+// go to top 
+$(window).scroll(function(){
+  if($(this).scrollTop() > 40){
+    $('#gotop').fadeIn();
+  } else{
+    $('#gotop').fadeOut();
+
+  }
+});
+
+$("#gotop").click(function(){
+$('html ,body').animate({scrollTop : 0}, 800);
+});
+  
+// go to top 
+
+// fake loader 
+$.fakeLoader({
+  timeToHide:1500,
+  bgColor:"#f7b926",
+  spinner:"spinner5"
+});
+
+});
